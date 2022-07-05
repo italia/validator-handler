fe6b236d28287dfd27238e6258fcdf4cb7a987d3
Andrea Cattaneo <a.cattaneo@accenture.com>
2022-07-05 15:22:04 +0200

    feat: Mappatura id-categorie audit crawler

---
+++ storage/municipalityAudits.ts
@@ -0,0 +1,31 @@
+export const modelComplianceInformationAudits = {
+  "municipality-menu-structure-match-model": "user-experience",
+  "municipality-second-level-pages": "user-experience",
+  "municipality-ux-ui-consistency-bootstrap-italia-double-check": "user-experience",
+  "municipality-informative-license-and-attribution": "legislation",
+  "municipality-informative-user-experience-evaluation": "function",
+  "municipality-legislation-cookie-domain-check": "legislation",
+  "municipality-security": "security",
+  "municipality-legislation-accessibility-declaration-is-present": "legislation",
+  "municipality-legislation-privacy-is-present": "legislation",
+  "municipality-faq-is-present": "function",
+  "municipality-inefficiency-report": "function",
+  "municipality-ux-ui-consistency-fonts-check": "user-experience",
+  "municipality-booking-appointment-check": "function",
+  "municipality-controlled-vocabularies": "user-experience",
+  "municipality-contacts-assistency": "function",
+  "municipality-feedback-element": "function",
+  "municipality-domain": "security",
+  "municipality-servizi-structure-match-model": "user-experience",
+  "municipality-ux-ui-consistency-theme-version-check": "user-experience"
+}
+
+export const modelComplianceActiveAudits = {
+  "municipality-personal-area-security": "security",
+  "municipality-subdomain": "security"
+}
+
+export const reccomandationsAndAdditionalTestsAudits = {
+  "municipality-informative-cloud-infrastructure": "legislation",
+  "municipality-informative-reuse": "legislation"
+}
---
+++ storage/schoolAudits.ts
@@ -0,0 +1,19 @@
+export const modelComplianceAudits = {
+  "school-ux-ui-consistency-fonts-check": "user-experience",
+  "school-ux-ui-consistency-bootstrap-italia-double-check": "user-experience",
+  "school-ux-ui-consistency-theme-version-check": "user-experience",
+  "school-menu-structure-match-model": "user-experience",
+  "school-menu-scuola-second-level-structure-match-model": "user-experience",
+  "school-legislation-cookie-domain-check": "legislation",
+  "school-legislation-accessibility-declaration-is-present": "legislation",
+  "school-legislation-privacy-is-present": "legislation",
+  "school-security": "security"
+}
+
+export const reccomendationsAudits = {
+  "school-servizi-structure-match-model": "user-experience",
+  "school-controlled-vocabularies": "user-experience",
+  "school-informative-reuse": "legislation",
+  "school-informative-license-and-attribution": "legislation",
+  "school-informative-cloud-infrastructure": "legislation"
+}
