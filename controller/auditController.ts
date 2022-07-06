e72e019abc0db3dd556f362eecfde3b5f1a2a4ef
Andrea Cattaneo <a.cattaneo@accenture.com>
2022-07-06 16:02:53 +0200

    feat: JSON KPI con riassunto scansione

---
+++ controller/auditController.ts
@@ -0,0 +1,313 @@
+"use strict";
+
+import { audits as municipalityAudits } from "../storage/municipalityAudits";
+import { audits as schoolAudits } from "../storage/schoolAudits";
+
+const cleanMunicipalityJSONReport = async (jsonResult: string) => {
+  const parsedResult = JSON.parse(jsonResult);
+
+  const userExperienceFailAudits = await getFailAuditByClusterGroup(
+    parsedResult,
+    municipalityAudits,
+    "informed-citizen",
+    "user-experience"
+  );
+  let userExperienceStatus = true;
+  if (userExperienceFailAudits.length > 0) {
+    userExperienceStatus = false;
+  }
+
+  const functionFailAudits = await getFailAuditByClusterGroup(
+    parsedResult,
+    municipalityAudits,
+    "informed-citizen",
+    "function"
+  );
+  let functionStatus = true;
+  if (functionFailAudits.length > 0) {
+    functionStatus = false;
+  }
+
+  const legislationFailAudits = await getFailAuditByClusterGroup(
+    parsedResult,
+    municipalityAudits,
+    "informed-citizen",
+    "legislation"
+  );
+  let legislationStatus = true;
+  if (legislationFailAudits.length > 0) {
+    legislationStatus = false;
+  }
+
+  const securityFailAudits = await getFailAuditByClusterGroup(
+    parsedResult,
+    municipalityAudits,
+    "informed-citizen",
+    "security"
+  );
+  let securityStatus = true;
+  if (securityFailAudits.length > 0) {
+    securityStatus = false;
+  }
+
+  const performanceScore = await getPerformanceScore(parsedResult);
+  let performanceStatus = true;
+  if (performanceScore < 0.5) {
+    performanceStatus = false;
+  }
+
+  let informedCitizenStatus = false;
+  if (
+    userExperienceStatus &&
+    functionStatus &&
+    legislationStatus &&
+    securityStatus &&
+    performanceStatus
+  ) {
+    informedCitizenStatus = true;
+  }
+
+  const activeCitizenFailAudits = await getFailAuditByClusterGroup(
+    parsedResult,
+    municipalityAudits,
+    "active-citizen"
+  );
+  let activeCitizenStatus = true;
+  if (activeCitizenFailAudits.length > 0) {
+    activeCitizenStatus = false;
+  }
+
+  const recommendationsFailAudits = await getFailAuditByClusterGroup(
+    parsedResult,
+    municipalityAudits,
+    "recommendations"
+  );
+  let recommendationsStatus = true;
+  if (recommendationsFailAudits.length > 0) {
+    recommendationsStatus = false;
+  }
+
+  return {
+    "cittadino-informato": {
+      status: informedCitizenStatus,
+
+      groups: {
+        "esperienza-utente": {
+          status: userExperienceStatus,
+          failAudit: userExperienceFailAudits,
+        },
+        funzionalita: {
+          status: functionStatus,
+          failAudit: functionFailAudits,
+        },
+        normativa: {
+          status: legislationStatus,
+          failAudit: legislationFailAudits,
+        },
+        sicurezza: {
+          status: securityStatus,
+          failAudit: securityFailAudits,
+        },
+        prestazioni: {
+          status: performanceStatus,
+        },
+      },
+    },
+
+    "cittadino-attivo": {
+      status: activeCitizenStatus,
+      failAudit: activeCitizenFailAudits,
+    },
+
+    raccomandazioni: {
+      status: recommendationsStatus,
+      failAudit: recommendationsFailAudits,
+    },
+  };
+};
+
+const cleanSchoolJSONReport = async (jsonResult: string) => {
+  const parsedResult = JSON.parse(jsonResult);
+
+  const userExperienceFailAudits = await getFailAuditByClusterGroup(
+    parsedResult,
+    schoolAudits,
+    "compliance-criteria",
+    "user-experience"
+  );
+  let userExperienceStatus = true;
+  if (userExperienceFailAudits.length > 0) {
+    userExperienceStatus = false;
+  }
+
+  const legislationFailAudits = await getFailAuditByClusterGroup(
+    parsedResult,
+    schoolAudits,
+    "compliance-criteria",
+    "legislation"
+  );
+  let legislationStatus = true;
+  if (legislationFailAudits.length > 0) {
+    legislationStatus = false;
+  }
+
+  const securityFailAudits = await getFailAuditByClusterGroup(
+    parsedResult,
+    schoolAudits,
+    "compliance-criteria",
+    "security"
+  );
+  let securityStatus = true;
+  if (securityFailAudits.length > 0) {
+    securityStatus = false;
+  }
+
+  const performanceScore = await getPerformanceScore(parsedResult);
+  let performanceStatus = true;
+  if (performanceScore < 0.5) {
+    performanceStatus = false;
+  }
+
+  let complianceCriteriaStatus = false;
+  if (
+    userExperienceStatus &&
+    legislationStatus &&
+    securityStatus &&
+    performanceStatus
+  ) {
+    complianceCriteriaStatus = true;
+  }
+
+  const recommendationsFailAudits = await getFailAuditByClusterGroup(
+    parsedResult,
+    schoolAudits,
+    "recommendations"
+  );
+  let recommendationsStatus = true;
+  if (recommendationsFailAudits.length > 0) {
+    recommendationsStatus = false;
+  }
+
+  return {
+    "criteri-conformita": {
+      status: complianceCriteriaStatus,
+
+      groups: {
+        "esperienza-utente": {
+          status: userExperienceStatus,
+          failAudit: userExperienceFailAudits,
+        },
+        normativa: {
+          status: legislationStatus,
+          failAudit: legislationFailAudits,
+        },
+        sicurezza: {
+          status: securityStatus,
+          failAudit: securityFailAudits,
+        },
+        prestazioni: {
+          status: performanceStatus,
+        },
+      },
+    },
+
+    raccomandazioni: {
+      status: recommendationsStatus,
+      failAudit: recommendationsFailAudits,
+    },
+  };
+};
+
+const getFailAuditByClusterGroup = async (
+  jsonResult,
+  audits,
+  cluster,
+  group = ""
+) => {
+  let auditsToGet: string[];
+  if (group !== "") {
+    auditsToGet = audits[cluster][group] ?? ["error"];
+  } else {
+    auditsToGet = audits[cluster] ?? ["error"];
+  }
+
+  if (!("audits" in jsonResult)) {
+    return auditsToGet;
+  }
+
+  const failAudits = [];
+  for (const audit of auditsToGet) {
+    let auditScore = 0;
+    if (
+      jsonResult.audits[audit] !== undefined &&
+      "score" in jsonResult.audits[audit]
+    ) {
+      auditScore = jsonResult.audits[audit].score ?? 0;
+    }
+
+    if (auditScore < 0.5) {
+      failAudits.push(audit);
+    }
+  }
+
+  return failAudits;
+};
+
+const getPerformanceScore = async (jsonResult) => {
+  if (!("categories" in jsonResult)) {
+    return 0;
+  }
+
+  if (!("performance" in jsonResult.categories)) {
+    return 0;
+  }
+
+  if (!("score" in jsonResult.categories.performance)) {
+    return 0;
+  }
+
+  return jsonResult.categories.performance.score;
+};
+
+const isPassedReport = async (jsonReport, type: string): Promise<boolean> => {
+  let passed = false;
+
+  switch (type) {
+    case "municipality":
+      // eslint-disable-next-line
+      const informedCitizenStatus = jsonReport["cittadino-informato"].status;
+
+      // eslint-disable-next-line
+      const activeCitizenStatus = jsonReport["cittadino-attivo"].status;
+
+      // eslint-disable-next-line
+      const recommendationsMunicipalityStatus =
+        jsonReport["raccomandazioni"].status;
+
+      if (
+        informedCitizenStatus &&
+        activeCitizenStatus &&
+        recommendationsMunicipalityStatus
+      ) {
+        passed = true;
+      }
+      break;
+    case "school":
+      // eslint-disable-next-line
+      const complianceCriteriaStatus = jsonReport["criteri-conformita"].status;
+
+      // eslint-disable-next-line
+      const recommendationsSchoolStatus = jsonReport["raccomandazioni"].status;
+
+      if (complianceCriteriaStatus && recommendationsSchoolStatus) {
+        passed = true;
+      }
+      break;
+    default:
+      passed = false;
+  }
+
+  return passed;
+};
+
+export { cleanMunicipalityJSONReport, cleanSchoolJSONReport, isPassedReport };
--- storage/schoolAudits.ts
+++ storage/schoolAudits.ts
@@ -1,19 +1,21 @@
-export const modelComplianceAudits = {
-  "school-ux-ui-consistency-fonts-check": "user-experience",
-  "school-ux-ui-consistency-bootstrap-italia-double-check": "user-experience",
-  "school-ux-ui-consistency-theme-version-check": "user-experience",
-  "school-menu-structure-match-model": "user-experience",
-  "school-menu-scuola-second-level-structure-match-model": "user-experience",
-  "school-legislation-cookie-domain-check": "legislation",
-  "school-legislation-accessibility-declaration-is-present": "legislation",
-  "school-legislation-privacy-is-present": "legislation",
-  "school-security": "security",
-};
-
-export const reccomendationsAudits = {
-  "school-servizi-structure-match-model": "user-experience",
-  "school-controlled-vocabularies": "user-experience",
-  "school-informative-reuse": "legislation",
-  "school-informative-license-and-attribution": "legislation",
-  "school-informative-cloud-infrastructure": "legislation",
+export const audits = {
+  "compliance-criteria": {
+    "user-experience": [
+      "school-ux-ui-consistency-fonts-check",
+      "school-ux-ui-consistency-bootstrap-italia-double-check",
+      "school-ux-ui-consistency-theme-version-check",
+      "school-menu-structure-match-model",
+      "school-menu-scuola-second-level-structure-match-model",
+    ],
+    legislation: [
+      "school-legislation-cookie-domain-check",
+      "school-legislation-accessibility-declaration-is-present",
+      "school-legislation-privacy-is-present",
+    ],
+    security: ["school-security"],
+  },
+  recommendations: [
+    "school-servizi-structure-match-model",
+    "school-controlled-vocabularies",
+  ],
 };
