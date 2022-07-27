014145b0ee20facdca1e64534641dcc6a0fe97dc
Andrea Cattaneo <a.cattaneo@accenture.com>
2022-07-27 17:43:46 +0200

    feat: Indici su campo json_result

--- database/models/job.ts
+++ database/models/job.ts
@@ -103,8 +103,112 @@ const structure: ModelAttributes<Job, Attributes<Job>> = {
   updatedAt: DataTypes.DATE,
 };
 
+const options = {
+  indexes: [
+    {
+      name: "index_json_result",
+      fields: [
+        Sequelize.literal(
+          "((json_result->'raccomandazioni'->'audits'->>'school-controlled-vocabularies')::float)"
+        ),
+        Sequelize.literal(
+          "((json_result->'raccomandazioni'->'audits'->>'school-servizi-structure-match-model')::float)"
+        ),
+        Sequelize.literal(
+          "((json_result->'criteri-conformita'->'groups'->'normativa'->'audits'->>'school-legislation-privacy-is-present')::float)"
+        ),
+        Sequelize.literal(
+          "((json_result->'criteri-conformita'->'groups'->'normativa'->'audits'->>'school-legislation-cookie-domain-check')::float)"
+        ),
+        Sequelize.literal(
+          "((json_result->'criteri-conformita'->'groups'->'normativa'->'audits'->>'school-legislation-accessibility-declaration-is-present')::float)"
+        ),
+        Sequelize.literal(
+          "((json_result->'criteri-conformita'->'groups'->'sicurezza'->'audits'->>'school-security')::float)"
+        ),
+        Sequelize.literal(
+          "((json_result->'criteri-conformita'->'groups'->'esperienza-utente'->'audits'->>'school-menu-structure-match-model')::float)"
+        ),
+        Sequelize.literal(
+          "((json_result->'criteri-conformita'->'groups'->'esperienza-utente'->'audits'->>'school-ux-ui-consistency-fonts-check')::float)"
+        ),
+        Sequelize.literal(
+          "((json_result->'criteri-conformita'->'groups'->'esperienza-utente'->'audits'->>'school-ux-ui-consistency-theme-version-check')::float)"
+        ),
+        Sequelize.literal(
+          "((json_result->'criteri-conformita'->'groups'->'esperienza-utente'->'audits'->>'school-menu-scuola-second-level-structure-match-model')::float)"
+        ),
+        Sequelize.literal(
+          "((json_result->'criteri-conformita'->'groups'->'esperienza-utente'->'audits'->>'school-ux-ui-consistency-bootstrap-italia-double-check')::float)"
+        ),
+
+        Sequelize.literal(
+          "((json_result->'raccomandazioni'->'audits'->>'municipality-metatag')::float)"
+        ),
+        Sequelize.literal(
+          "((json_result->'cittadino-attivo'->'audits'->>'municipality-subdomain')::float)"
+        ),
+        Sequelize.literal(
+          "((json_result->'cittadino-attivo'->'audits'->>'municipality-personal-area-security')::float)"
+        ),
+        Sequelize.literal(
+          "((json_result->'cittadino-informato'->'groups'->'normativa'->'audits'->>'municipality-legislation-privacy-is-present')::float)"
+        ),
+        Sequelize.literal(
+          "((json_result->'cittadino-informato'->'groups'->'normativa'->'audits'->>'municipality-legislation-cookie-domain-check')::float)"
+        ),
+        Sequelize.literal(
+          "((json_result->'cittadino-informato'->'groups'->'normativa'->'audits'->>'municipality-legislation-accessibility-declaration-is-present')::float)"
+        ),
+        Sequelize.literal(
+          "((json_result->'cittadino-informato'->'groups'->'sicurezza'->'audits'->>'municipality-domain')::float)"
+        ),
+        Sequelize.literal(
+          "((json_result->'cittadino-informato'->'groups'->'sicurezza'->'audits'->>'municipality-security')::float)"
+        ),
+        Sequelize.literal(
+          "((json_result->'cittadino-informato'->'groups'->'funzionalita'->'audits'->>'municipality-faq-is-present')::float)"
+        ),
+        Sequelize.literal(
+          "((json_result->'cittadino-informato'->'groups'->'funzionalita'->'audits'->>'municipality-feedback-element')::float)"
+        ),
+        Sequelize.literal(
+          "((json_result->'cittadino-informato'->'groups'->'funzionalita'->'audits'->>'municipality-contacts-assistency')::float)"
+        ),
+        Sequelize.literal(
+          "((json_result->'cittadino-informato'->'groups'->'funzionalita'->'audits'->>'municipality-inefficiency-report')::float)"
+        ),
+        Sequelize.literal(
+          "((json_result->'cittadino-informato'->'groups'->'funzionalita'->'audits'->>'municipality-booking-appointment-check')::float)"
+        ),
+        Sequelize.literal(
+          "((json_result->'cittadino-informato'->'groups'->'esperienza-utente'->'audits'->>'municipality-second-level-pages')::float)"
+        ),
+        Sequelize.literal(
+          "((json_result->'cittadino-informato'->'groups'->'esperienza-utente'->'audits'->>'municipality-controlled-vocabularies')::float)"
+        ),
+        Sequelize.literal(
+          "((json_result->'cittadino-informato'->'groups'->'esperienza-utente'->'audits'->>'municipality-menu-structure-match-model')::float)"
+        ),
+        Sequelize.literal(
+          "((json_result->'cittadino-informato'->'groups'->'esperienza-utente'->'audits'->>'municipality-servizi-structure-match-model')::float)"
+        ),
+        Sequelize.literal(
+          "((json_result->'cittadino-informato'->'groups'->'esperienza-utente'->'audits'->>'municipality-ux-ui-consistency-fonts-check')::float)"
+        ),
+        Sequelize.literal(
+          "((json_result->'cittadino-informato'->'groups'->'esperienza-utente'->'audits'->>'municipality-ux-ui-consistency-theme-version-check')::float)"
+        ),
+        Sequelize.literal(
+          "((json_result->'cittadino-informato'->'groups'->'esperienza-utente'->'audits'->>'municipality-ux-ui-consistency-bootstrap-italia-double-check')::float)"
+        ),
+      ],
+    },
+  ],
+};
+
 const syncTable = (db: Sequelize) => {
-  return db.define(modelName, structure).sync({ alter: true });
+  return db.define(modelName, structure, options).sync({ alter: true });
 };
 
 const define = (db: Sequelize, join = true) => {
