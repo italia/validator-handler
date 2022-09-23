71f6462762c2187d46b6d4c776f48084bfad6e6f
Andrea Cattaneo <a.cattaneo@accenture.com>
2022-09-23 15:31:28 +0200

    fix: aggiunta region in AWS configuration

--- controller/s3Controller.ts
+++ controller/s3Controller.ts
@@ -8,6 +8,7 @@ import AWS from "aws-sdk";
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
 });
 
 const upload = async (fileContent: string, bucketFolderPath: string) => {
