2b97b91fb6dad21329d41084998681ddbc88320c
Andrea Cattaneo <a.cattaneo@accenture.com>
2023-05-18 18:43:13 +0200

    fix: scanManagerItem implementation

--- command/scanManager.ts
+++ command/scanManager.ts
@@ -3,32 +3,25 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import Redis from "ioredis";
 import { dbSM, dbWS } from "../database/connection";
 import { define as jobDefine } from "../database/models/job";
-import { run } from "pa-website-validator/dist/controller/launchLighthouse";
-import { logLevels } from "pa-website-validator/dist/controller/launchLighthouse";
 import { Entity, Job } from "../types/models";
-import {
-  upload as s3Upload,
-  empty as s3Delete,
-} from "../controller/s3Controller";
 import { Worker, Job as bullJob } from "bullmq";
 import { v4 } from "uuid";
-import {
-  cleanMunicipalityJSONReport,
-  cleanSchoolJSONReport,
-  isPassedReport,
-} from "../controller/auditController";
-import { jobController } from "../controller/jobController";
-import {
-  pushResult,
-  pushResultUrlNotExists,
-} from "../controller/PA2026/integrationController";
-import { urlExists } from "../utils/utils";
 import { entityController } from "../controller/entityController";
-import psList from "ps-list";
-import treeKill from "tree-kill";
+import { spawnSync } from "child_process";
+
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import Redis from "ioredis";
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+const __filenameExtension = path.extname(fileURLToPath(import.meta.url));
 
 dbSM
   .authenticate()
@@ -47,32 +40,55 @@ dbSM
     });
     const token = v4();
     let job: bullJob;
-
     while ((job = await worker.getNextJob(token)) !== undefined) {
-      console.log("JOB START FOR jobID: ", job.data.id);
-      const result = await scan(job.data.id);
+      try {
+        console.log("[SCAN MANAGER] SPAWN START..");
+        if (!__filenameExtension) {
+          throw new Error("Filename extension not found");
+        }
 
-      const jobObj: Job | null = await jobDefine(dbSM).findByPk(job.data.id);
-      if (!jobObj) {
-        continue;
-      }
+        const command =
+          `node --max-old-space-size=8192 --no-warnings --experimental-modules --es-module-specifier-resolution=node --loader ts-node/esm ${__dirname}/scanManagerItem${__filenameExtension} --jobId ` +
+          job.data.id;
 
-      const entity: Entity | null = await new entityController(
-        dbWS
-      ).retrieveById(jobObj.entity_id);
+        console.log("[SCAN MANAGER] EXECUTING: ", command);
 
-      if (!entity) {
-        continue;
-      }
+        const child = spawnSync(command, {
+          shell: true,
+        });
+        console.log(
+          "[SCAN MANAGER] LOG FROM SCAN-MANAGER-ITEM:",
+          child.stdout.toString()
+        );
+        console.log(
+          "[SCAN MANAGER] STATUS FROM SCAN-MANAGER-ITEM",
+          child.status
+        );
 
-      await entity.update({
-        forcedScan: false,
-      });
+        const jobObj: Job | null = await jobDefine(dbSM).findByPk(job.data.id);
+        if (!jobObj) {
+          throw new Error("Empty job");
+        }
 
-      if (result) {
-        await job.moveToCompleted("completed", token, false);
-      } else {
-        await job.moveToFailed(new Error("error"), token);
+        const entity: Entity | null = await new entityController(
+          dbWS
+        ).retrieveById(jobObj.entity_id);
+
+        if (!entity) {
+          throw new Error("Empty entity");
+        }
+
+        await entity.update({
+          forcedScan: false,
+        });
+
+        if (child.status === 0) {
+          await job.moveToCompleted("completed", token, false);
+        } else {
+          await job.moveToFailed(new Error("error"), token);
+        }
+      } catch (e) {
+        console.log("[ERROR]: SCAN MANAGER WHILE EXCEPTION: ", e);
       }
     }
 
@@ -82,202 +98,6 @@ dbSM
     process.exit(0);
   })
   .catch((err) => {
-    console.error("Error: ", err);
+    console.error("[SCAN MANAGER] - Error: ", err);
     process.exit(1);
   });
-
-const scan = async (jobId) => {
-  const jobObj: Job | null = await jobDefine(dbSM).findByPk(jobId);
-
-  try {
-    if (jobObj === null || jobObj.toJSON().status !== "PENDING") {
-      return false;
-    }
-
-    await jobObj.update({
-      status: "IN_PROGRESS",
-      start_at: Date.now(),
-    });
-
-    const jobObjParsed = jobObj.toJSON();
-    const urlToBeScanned = jobObjParsed.scan_url;
-
-    const urlToBeScannedExists = await urlExists(urlToBeScanned);
-    if (!urlToBeScannedExists) {
-      await pushResultUrlNotExists(jobObj, urlToBeScanned);
-      throw new Error("Scan URL does not exists");
-    }
-
-    const lighthouseResult = await run(
-      urlToBeScanned,
-      jobObjParsed.type,
-      "online",
-      logLevels.display_none,
-      false,
-      "",
-      "",
-      false,
-      "all"
-    );
-
-    if (!lighthouseResult.status) {
-      throw new Error("Empty lighthouse result");
-    }
-
-    let jsonResult = {};
-    if (jobObjParsed.type === "municipality") {
-      jsonResult = await cleanMunicipalityJSONReport(
-        lighthouseResult.data.jsonReport
-      );
-    } else if (jobObjParsed.type === "school") {
-      jsonResult = await cleanSchoolJSONReport(
-        lighthouseResult.data.jsonReport
-      );
-    }
-
-    const uploadResult = await uploadFiles(
-      jobObjParsed.id,
-      jobObjParsed.entity_id,
-      lighthouseResult.data.htmlReport,
-      lighthouseResult.data.jsonReport,
-      JSON.stringify(jsonResult)
-    );
-
-    if (!uploadResult.status) {
-      throw new Error("Upload error");
-    }
-
-    const status = await isPassedReport(
-      jsonResult,
-      jobObjParsed.type,
-      jobObjParsed.entity_id
-    );
-
-    const job: Job = await jobObj.update({
-      status: status ? "PASSED" : "FAILED",
-      end_at: Date.now(),
-      json_result: jsonResult,
-      s3_json_url: uploadResult.jsonLocationUrl,
-      s3_html_url: uploadResult.htmlLocationUrl,
-      s3_clean_json_result_url: uploadResult.cleanJsonLocationUrl,
-    });
-
-    if (!job) {
-      throw new Error("Update job failed");
-    }
-
-    await pushResult(job, jsonResult, status, lighthouseResult.data.htmlReport);
-
-    const jobDeleted = await new jobController(dbSM).cleanJobs(
-      jobObjParsed.entity_id
-    );
-    console.log("JOB DELETED: ", jobDeleted);
-
-    const pidKilled = await killProcessByName("Chromium");
-    console.log("PID KILLED: ", pidKilled);
-
-    return true;
-  } catch (e) {
-    console.log("SCAN EXCEPTION: ", e.toString());
-
-    await jobObj.update({
-      status: "ERROR",
-      end_at: Date.now(),
-    });
-
-    const pidKilled = await killProcessByName("Chromium");
-    console.log("PID KILLED: ", pidKilled);
-
-    return false;
-  }
-};
-
-const uploadFiles = async (
-  jobId: number,
-  entityId: number,
-  htmlReport: string,
-  jsonReport: string,
-  cleanJsonReport: string
-): Promise<{
-  status: boolean;
-  htmlLocationUrl: string | null;
-  jsonLocationUrl: string | null;
-  cleanJsonLocationUrl: string | null;
-}> => {
-  try {
-    if (parseInt(process.env.BYPASS_S3_UPLOAD) === 1) {
-      return {
-        status: true,
-        htmlLocationUrl: "/" + entityId + "/" + jobId + "/" + "report.html",
-        jsonLocationUrl: "/" + entityId + "/" + jobId + "/" + "report.json",
-        cleanJsonLocationUrl:
-          "/" + entityId + "/" + jobId + "/" + "summary.json",
-      };
-    }
-
-    const htmlLocationUrl = await s3Upload(
-      htmlReport,
-      entityId + "/" + jobId + "/" + "report.html"
-    );
-    const jsonLocationUrl = await s3Upload(
-      jsonReport,
-      entityId + "/" + jobId + "/" + "report.json"
-    );
-    const cleanJsonLocationUrl = await s3Upload(
-      cleanJsonReport,
-      entityId + "/" + jobId + "/" + "summary.json"
-    );
-
-    if (
-      htmlLocationUrl === null ||
-      jsonLocationUrl === null ||
-      cleanJsonLocationUrl === null
-    ) {
-      throw new Error("Empty result from S3");
-    }
-
-    return {
-      status: true,
-      htmlLocationUrl: htmlLocationUrl,
-      jsonLocationUrl: jsonLocationUrl,
-      cleanJsonLocationUrl: cleanJsonLocationUrl,
-    };
-  } catch (ex) {
-    if (Boolean(entityId) && Boolean(jobId)) {
-      await s3Delete(entityId + "/" + jobId);
-    }
-
-    return {
-      status: false,
-      htmlLocationUrl: null,
-      jsonLocationUrl: null,
-      cleanJsonLocationUrl: null,
-    };
-  }
-};
-
-const killProcessByName = async (name: string) => {
-  const pidKilled = [];
-
-  try {
-    const list = await psList();
-
-    for (const element of list) {
-      try {
-        if (element.name === name) {
-          await treeKill(element.pid);
-          pidKilled.push(element.pid);
-        }
-      } catch (e) {
-        console.log("ELEMENT IN ERROR: ", element);
-        console.log("KILL PROCESS BY NAME FOR-STATEMENT EXCEPTION: ", e);
-      }
-    }
-
-    return pidKilled;
-  } catch (e) {
-    console.log("KILL PROCESS BY NAME EXCEPTION: ", e);
-
-    return pidKilled;
-  }
-};
