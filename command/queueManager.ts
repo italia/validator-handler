775b5bf82af030e5d7bafe464ce119b8e409f6d4
Simone Amadio <simone.amadio@accenture.com>
2025-02-25 17:51:16 +0100

    feat: remove schedule rescan

--- command/queueManager.ts
+++ command/queueManager.ts
@@ -12,57 +12,15 @@ import { Queue } from "bullmq";
 import { preserveReasons } from "../database/models/job.js";
 import { jobController } from "../controller/jobController.js";
 import {
-  getFirstTimeEntityToBeAnalyzed,
-  getRescanEntityToBeAnalyzed,
-  getRescanEntityAsseveratedToBeAnalyzed,
   generateJobs,
   getForcedRescanEntitiesToBeAnalyzed,
   getFirstTimeForcedEntityToBeAnalyzed,
   manageEntitiesInErrorJobs,
 } from "../controller/queueManagerController.js";
 
-const command = yargs(hideBin(process.argv))
-  .usage(
-    "Usage: " +
-      "--maxItems <maxItems> " +
-      "--passedOlderThanDays <passedOlderThanDays> " +
-      "--failedOlderThanDays <failedOlderThanDays> " +
-      "--manualScanLogic"
-  )
-  .option("maxItems", {
-    describe: "Numero massimo di entity da analizzare",
-    type: "integer",
-    demandOption: true,
-    default: 100,
-  })
-  .option("passedOlderThanDays", {
-    describe:
-      "Giorni dopo i quali le entity con Job che ha fornito risultato PASSED vengono riaccodate per essere scansionate",
-    type: "integer",
-    demandOption: true,
-    default: 28,
-  })
-  .option("failedOlderThanDays", {
-    describe:
-      "Giorni dopo i quali le entity con Job che ha fornito risultato FAILED vengono riaccodate per essere scansionate",
-    type: "integer",
-    demandOption: true,
-    default: 14,
-  })
-  .option("asservationOlderThanDays", {
-    describe:
-      "Giorni dopo i quali le entity asseverate vengono riaccodate per essere scansionate",
-    type: "integer",
-    demandOption: true,
-    default: 28,
-  })
-  .option("manualScanLogic", {
-    describe:
-      "Flag per permettere solo alle entity flaggate come 'da scansionare' di entrare in coda di scansione",
-    type: "bool",
-    demandOption: true,
-    default: false,
-  }).argv;
+const command = yargs(hideBin(process.argv)).usage(
+  "Usage: " + "--maxItems <maxItems> "
+).argv;
 
 dbQM
   .authenticate()
@@ -96,30 +54,17 @@ dbQM
     );
     console.log('MANAGE JOB IN "PENDING": ', inPendingJob.length);
 
-    const manualScanLogic = command.manualScanLogic;
-
     let entityInErrorJob = [];
-    if (manualScanLogic) {
-      entityInErrorJob = await manageEntitiesInErrorJobs();
-      console.log(
-        'MANAGE ENTITY WITH LAST JOB IN "ERROR": ',
-        entityInErrorJob.length
-      );
-    }
+    entityInErrorJob = await manageEntitiesInErrorJobs();
+    console.log(
+      'MANAGE ENTITY WITH LAST JOB IN "ERROR": ',
+      entityInErrorJob.length
+    );
 
     let gapLimit: number = parseInt(command.maxItems);
 
-    let firstTimeEntityToBeAnalyzed = [];
-    if (command.passedOlderThanDays > 0 || command.failedOlderThanDays) {
-      firstTimeEntityToBeAnalyzed = await getFirstTimeEntityToBeAnalyzed(
-        gapLimit
-      );
-
-      gapLimit = gapLimit - firstTimeEntityToBeAnalyzed.length;
-    }
-
     let firstTimeForcedEntityToBeAnalyzed = [];
-    if (gapLimit > 0 && manualScanLogic) {
+    if (gapLimit > 0) {
       firstTimeForcedEntityToBeAnalyzed =
         await getFirstTimeForcedEntityToBeAnalyzed(gapLimit);
 
@@ -127,76 +72,37 @@ dbQM
     }
 
     let forcedRescanEntitiesToBeAnalyzed = [];
-    if (gapLimit > 0 && manualScanLogic) {
+    if (gapLimit > 0) {
       forcedRescanEntitiesToBeAnalyzed =
         await getForcedRescanEntitiesToBeAnalyzed(gapLimit);
 
       gapLimit = gapLimit - forcedRescanEntitiesToBeAnalyzed.length;
     }
 
-    let rescanEntityToBeAnalyzed = [];
-    if (
-      gapLimit > 0 &&
-      (command.passedOlderThanDays > 0 || command.failedOlderThanDays)
-    ) {
-      rescanEntityToBeAnalyzed = await getRescanEntityToBeAnalyzed(
-        command.passedOlderThanDays,
-        command.failedOlderThanDays,
-        gapLimit
-      );
-
-      gapLimit = gapLimit - rescanEntityToBeAnalyzed.length;
-    }
-
-    let rescanEntityAsseveratedToBeAnalyzed = [];
-    if (gapLimit > 0 && command.asservationOlderThanDays > 0) {
-      rescanEntityAsseveratedToBeAnalyzed =
-        await getRescanEntityAsseveratedToBeAnalyzed(
-          command.asservationOlderThanDays,
-          gapLimit
-        );
-    }
-
-    if (
-      firstTimeEntityToBeAnalyzed.length > 0 ||
-      firstTimeForcedEntityToBeAnalyzed.length > 0
-    ) {
+    if (firstTimeForcedEntityToBeAnalyzed.length > 0) {
       await generateJobs(
-        [...firstTimeEntityToBeAnalyzed, ...firstTimeForcedEntityToBeAnalyzed],
+        [...firstTimeForcedEntityToBeAnalyzed],
         crawlerQueue,
         true,
         preserveReasons[0]
       );
     }
 
-    rescanEntityToBeAnalyzed = rescanEntityToBeAnalyzed.filter(
-      (elem) => !firstTimeEntityToBeAnalyzed.find(({ id }) => elem.id === id)
+    forcedRescanEntitiesToBeAnalyzed = forcedRescanEntitiesToBeAnalyzed.filter(
+      (elem) =>
+        !firstTimeForcedEntityToBeAnalyzed.find(({ id }) => elem.id === id)
     );
 
-    rescanEntityAsseveratedToBeAnalyzed =
-      rescanEntityAsseveratedToBeAnalyzed.filter(
-        (elem) => !firstTimeEntityToBeAnalyzed.find(({ id }) => elem.id === id)
-      );
-
     if (
-      rescanEntityToBeAnalyzed.length > 0 ||
-      rescanEntityAsseveratedToBeAnalyzed.length > 0 ||
       forcedRescanEntitiesToBeAnalyzed.length > 0 ||
       entityInErrorJob.length > 0
     ) {
       await generateJobs(
-        [
-          ...rescanEntityToBeAnalyzed,
-          ...rescanEntityAsseveratedToBeAnalyzed,
-          ...forcedRescanEntitiesToBeAnalyzed,
-          ...entityInErrorJob,
-        ],
+        [...forcedRescanEntitiesToBeAnalyzed, ...entityInErrorJob],
         crawlerQueue,
         false
       );
     }
-
-    console.log("FIRST TIME ENTITIES", firstTimeEntityToBeAnalyzed.length);
 
     console.log(
       "FIRST TIME FORCED ENTITIES",
@@ -206,14 +112,6 @@ dbQM
     console.log(
       "FORCED RESCAN ENTITIES",
       forcedRescanEntitiesToBeAnalyzed.length
-    );
-    console.log(
-      "RESCAN ENTITIES TO BE ANALYZED",
-      rescanEntityToBeAnalyzed.length
-    );
-    console.log(
-      "RESCAN ASSEVERATED ENTITIES",
-      rescanEntityAsseveratedToBeAnalyzed.length
     );
 
     const counts = await crawlerQueue.getJobCounts(
